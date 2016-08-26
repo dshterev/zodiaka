9b555e4952a249236d7e18bf12fbea14b5b2240d
dshterev <dg.shterev@gmail.com>
2016-08-26 13:31:35 +0300

    Update login.php

--- SoftUni-Blog/views/users/login.php
+++ SoftUni-Blog/views/users/login.php
@@ -2,8 +2,12 @@
 
 <h1><?= htmlspecialchars($this->title) ?></h1>
 
-<form method="post">
-    <div>Username: <input type="text" name="username"></div>
-    <div>Password: <input type="password" name="password"></div>
-    <div><input type="submit" value="Login"></div>
+<form method="post" id="user-form">
+    <label for="user-name">Username:</label>
+    <input type="text" name="username" id="user-name">
+
+    <label for="user-password">Password: </label>
+    <input type="password" name="password" id="user-password">
+
+    <input type="submit" value="Login">
 </form>
