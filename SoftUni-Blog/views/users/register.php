5008e230e4867d16749aa0fe064f04b94dad2876
dshterev <dg.shterev@gmail.com>
2016-08-26 13:32:00 +0300

    Update register.php

--- SoftUni-Blog/views/users/register.php
+++ SoftUni-Blog/views/users/register.php
@@ -2,10 +2,22 @@
 
 <h1><?= htmlspecialchars($this->title) ?></h1>
 
-<form method="post">
-    <div>Username: <input type="text" name="username" required></div>
-    <div>Password: <input type="password" name="password" required></div>
-    <div>Password Confirm: <input type="password" name="password_confirm" required></div>
-    <div>Fullname: <input type="text" name="full_name"></div>
-    <div><input type="submit" value="Login"></div>
+<form method="post" id="register-form">
+    <label for="register-username">Username:</label>
+    <input type="text" name="username" id="register-username" required>
+    </br>
+    
+    <label for="register-password">Password:</label>
+    <input type="password" name="password" id="register-password" required>
+    </br>
+    
+    <label for="register-user-confirm">Password Confirm:</label>
+    <input type="password" name="password_confirm" id="register-user-confirm" required>
+    </br>
+    
+    <label for="register-user-fullname">Fullname:</label>
+    <input type="text" name="full_name" id="register-user-fullname">
+    </br>
+    
+    <input type="submit" value="Login">
 </form>
